813e1643fce32b3e660ec33df844959711883c0f
L1m3Kun <sungku2757@gmail.com>
2024-12-17 01:01:21 +0900

    feat: 사인 위치 조정 및 버튼 디자인 & ref 위치 부모요소로 변경

--- src/Canvas/SignatureCanvas.tsx
+++ src/Canvas/SignatureCanvas.tsx
@@ -1,26 +1,33 @@
 import useDraw from "./useDraw";
-import SignBG from "../assets/images/사인칸.png";
+import { RefObject } from "react";
 
-const SignatureCanvas = () => {
-  const { canvasRef, clearCanvas, draw, startDrawing, stopDrawing } = useDraw();
+interface SignatureCanvasProps {
+  reff: RefObject<HTMLCanvasElement>;
+}
+
+const SignatureCanvas = ({ reff }: SignatureCanvasProps) => {
+  const { canvasRef, clearCanvas, draw, startDrawing, stopDrawing } = useDraw({
+    canvasRef: reff,
+  });
   return (
-    <div>
+    <div className="flex justify-center ml-[7.4rem] gap-2">
       <canvas
+        className="border-solid border cursor-crosshair bg-sign-img bg-cover"
         ref={canvasRef}
-        width={600}
-        height={400}
-        style={{
-          border: "1px solid black",
-          cursor: "crosshair",
-        }}
+        width={380}
+        height={200}
         onMouseDown={startDrawing}
         onMouseMove={draw}
         onMouseUp={stopDrawing}
         onMouseLeave={stopDrawing}
+      ></canvas>
+      <button
+        type="button"
+        onClick={clearCanvas}
+        className="h-10 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
       >
-        <img src={SignBG} alt="사인칸" />
-      </canvas>
-      <button onClick={clearCanvas}>Clear</button>
+        다시 그리기
+      </button>
     </div>
   );
 };
