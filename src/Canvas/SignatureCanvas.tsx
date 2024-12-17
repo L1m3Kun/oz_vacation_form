e11156e5f77abcbb708bc24ff5d160756916c2e3
L1m3Kun <sungku2757@gmail.com>
2024-12-17 13:33:41 +0900

    fix: 서명란 크기 및 버튼 수정

--- src/Canvas/SignatureCanvas.tsx
+++ src/Canvas/SignatureCanvas.tsx
@@ -10,25 +10,30 @@ const SignatureCanvas = ({ reff }: SignatureCanvasProps) => {
     canvasRef: reff,
   });
   return (
-    <div className="flex justify-center ml-[7.4rem] gap-2">
-      <canvas
-        className="border-solid border cursor-crosshair bg-sign-img bg-cover"
-        ref={canvasRef}
-        width={380}
-        height={200}
-        onMouseDown={startDrawing}
-        onMouseMove={draw}
-        onMouseUp={stopDrawing}
-        onMouseLeave={stopDrawing}
-      ></canvas>
-      <button
-        type="button"
-        onClick={clearCanvas}
-        className="h-10 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-      >
-        다시 그리기
-      </button>
-    </div>
+    <>
+      <label htmlFor="sign" className="mx-auto">
+        서명
+      </label>
+      <div className="flex justify-center mx-auto gap-4">
+        <canvas
+          className="border-solid border cursor-crosshair bg-sign-img bg-cover"
+          ref={canvasRef}
+          width={"320rem"}
+          height={"160rem"}
+          onMouseDown={startDrawing}
+          onMouseMove={draw}
+          onMouseUp={stopDrawing}
+          onMouseLeave={stopDrawing}
+        ></canvas>
+        <button
+          type="button"
+          onClick={clearCanvas}
+          className="flex justify-center align-middle w-10 h-10 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+        >
+          🔄
+        </button>
+      </div>
+    </>
   );
 };
 
