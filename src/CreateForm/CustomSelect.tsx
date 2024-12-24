ea18bcc9d7ff200193e46ecda49794ae890aa714
L1m3Kun <sungku2757@gmail.com>
2024-12-24 17:56:29 +0900

    fix: 캠프명 수정 사항 오류 수정, 캠프명 추가 반영 안됨

--- src/CreateForm/CustomSelect.tsx
+++ src/CreateForm/CustomSelect.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from "react";
+import { useVacation } from "../context/VacationContext";
 
 const TRACKS = [
   {
@@ -7,21 +7,16 @@ const TRACKS = [
   },
   { id: "BE", value: "초격차 웹 개발 캠프(백엔드)" },
   { id: "FS", value: "관리형 웹 풀스택 부트캠프" },
+  { id: "CEO", value: "CEO 개발부트캠프" },
+  { id: "DS", value: "디자인 부트캠프" },
 ];
 
-interface CustomSelectProps {
-  track:
-    | "초격차 웹 개발 캠프(프론트엔드)"
-    | "초격차 웹 개발 캠프(백엔드)"
-    | "관리형 웹 풀스택 부트캠프";
-  onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
-}
-
-const CustomSelect = ({ track, onChange }: CustomSelectProps) => {
+const CustomSelect = () => {
+  const { track, handleChangeInput } = useVacation();
   return (
     <select
       defaultValue={track}
-      onChange={onChange}
+      onChange={handleChangeInput}
       className="text-black w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm bg-white focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-200"
       name="track"
       id="track"
