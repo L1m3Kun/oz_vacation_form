5ccb78405e71ab12f14c1aeca7ebeea4b9663a27
강성구 <sungku2757@gmail.com>
2025-03-06 20:51:33 +0900

    refact: pdf 다운로드 함수 최적화

--- src/components/VacationForm/Vacation.Mobile.tsx
+++ src/components/VacationForm/Vacation.Mobile.tsx
@@ -1,6 +1,6 @@
 import { usePDF } from "@react-pdf/renderer";
 
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import CustomButton from "../../common/CustomButton";
 
 interface VacationMobileProps {
@@ -10,21 +10,7 @@ interface VacationMobileProps {
 const VacationMobile = ({ documentS }: VacationMobileProps) => {
   const [documentInstance] = usePDF({ document: documentS });
 
-  const makeUrlFromPDF = () => {
-    if (documentInstance.blob) {
-      const url = URL.createObjectURL(documentInstance.blob);
-      const aTag = document.createElement("a");
-      aTag.href = url;
-      aTag.target = "_blank";
-      aTag.download = `휴가신청서.pdf`;
-      document.body.appendChild(aTag);
-      aTag.click();
-      document.body.removeChild(aTag);
-      URL.revokeObjectURL(url);
-    }
-  };
-
-  useEffect(() => {
+  const makeUrlFromPDF = useCallback(() => {
     if (documentInstance.blob) {
       const url = URL.createObjectURL(documentInstance.blob);
       const aTag = document.createElement("a");
@@ -38,8 +24,12 @@ const VacationMobile = ({ documentS }: VacationMobileProps) => {
     }
   }, [documentInstance]);
 
+  useEffect(() => {
+    makeUrlFromPDF();
+  }, [makeUrlFromPDF]);
+
   return (
-    <div className="h-screen w-full text-center flex flex-col justify-center items-center gap-8">
+    <div className="h-full w-full text-center flex flex-col justify-center items-center gap-8">
       <p className="font-semibold">
         모바일 환경에서는 미리보기가 제공되지 않습니다.
       </p>
