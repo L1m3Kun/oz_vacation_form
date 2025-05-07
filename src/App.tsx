0e0fa4d7dc9544b68906c9910ee93f69ac410745
강성구 <sungku2757@gmail.com>
2025-05-07 11:08:02 +0900

    feat: console word added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import CreatingFormController from "./components/CreateForm/CreatingFormController";
 
 function App() {
+  console.log(` 
+ ██████╗ ███████╗
+██╔═══██╗╚══███╔╝
+██║   ██║  ███╔╝ 
+██║   ██║ ███╔╝  
+╚██████╔╝███████╗
+ ╚═════╝ ╚══════╝
+
+OZ VACATION FORM
+`);
   return (
     <>
       <CreatingFormController />
