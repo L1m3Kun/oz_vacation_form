f8a48a8ba176bcb3078bfcdfa41fe5846496983d
L1m3Kun <sungku2757@gmail.com>
2025-10-16 18:17:47 +0900

    fix: 프론트엔드 15기 17차수 적용 및 1인 창업가 3기 적용

--- src/utils/translateText.ts
+++ src/utils/translateText.ts
@@ -14,17 +14,33 @@ export const useTranslateText = () => {
     }
   };
 
-  const translateFlag = (
+  const translateTrackInfo = (
     track: InputValueType["track"],
     flag: InputValueType["flag"]
   ) => {
-    if (track === "초격차 캠프 프론트엔드 코스" && Number(flag) >= 9) {
-      return (Number(flag) + 1).toString();
+    if (track === "초격차 캠프 프론트엔드 코스") {
+      if (Number(flag) >= 15) {
+        return { track, flag: (Number(flag) + 2).toString() };
+      }
+      if (Number(flag) >= 9) {
+        return { track, flag: (Number(flag) + 1).toString() };
+      }
+      return { track, flag };
     }
     if (track === "1인 창업가 개발부트캠프") {
-      return (Number(flag) + 1).toString();
+      if (Number(flag) < 3) {
+        return {
+          track: "IT스타트업 실무형 풀스택 웹 개발 부트캠프 (React + Node.js)",
+          flag: (Number(flag) + 1).toString(),
+        };
+      }
+      if (Number(flag) === 3) {
+        return { track: "초격차 캠프 프론트엔드 코스", flag: "16" };
+      }
+      return { track, flag };
     }
-    return flag;
+
+    return { track, flag };
   };
-  return { translateFlag, translateTrack };
+  return { translateTrackInfo, translateTrack };
 };
